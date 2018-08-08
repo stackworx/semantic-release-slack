2bebd4c2df8de5b8d5b02661f3836a85b8b19c4e
cliedeman <ciaranliedeman@gmail.com>
2018-08-08 17:01:58 +0200

    Fixed success

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-const successSlack = require("./lib/success");
+const successSlack = require('./lib/success');
 
 async function success(pluginConfig, context) {
-  if (!verified) {
-    await verifyGitHub(pluginConfig, context);
-    verified = true;
-  }
   await successSlack(pluginConfig, context);
 }
 
-module.exports = { success };
+module.exports = {success};
